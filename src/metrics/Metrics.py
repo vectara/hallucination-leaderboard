bc750b880359c7a5fa021f7f4a774653a64ec2fa
ahmed <ahmed@vectara.com>
2025-05-27 13:27:23 -0700

    Proper style adjustments

--- src/metrics/Metrics.py
+++ src/metrics/Metrics.py
@@ -3,7 +3,12 @@ class Metrics:
     def __init__(self):
         pass
 
-    def compute_hallucination_rate(self, hhem_scores, threshold=0.5):
+    def compute_hallucination_rate(
+            self, hhem_scores: list[float], threshold=0.5
+        ):
+        """
+        
+        """
         factual_consistancy_rate = self.compute_factual_consistancy_rate(
             hhem_scores, threshold=threshold
         )
@@ -11,13 +16,21 @@ class Metrics:
         hallucination_rate = round(hallucination_rate, 1)
         return hallucination_rate
 
-    def compute_factual_consistancy_rate(self, hhem_scores, threshold=0.5):
+    def compute_factual_consistancy_rate(
+            self, hhem_scores: list[float], threshold=0.5
+        ):
+        """
+        
+        """
         factual_count = sum(score >= threshold for score in hhem_scores)
         factual_consistancy_rate = factual_count/len(hhem_scores)
         factual_consistancy_rate = round(factual_consistancy_rate, 1)
         return factual_consistancy_rate
 
-    def compute_answer_rate(self, summaries):
+    def compute_answer_rate(self, summaries: list[str]):
+        """
+        
+        """
         valid_summ_count = sum(
             self.is_valid_summary(summary) for summary in summaries
         )
@@ -25,7 +38,10 @@ class Metrics:
         answer_rate = round(answer_rate, 1)
         return answer_rate
 
-    def compute_avg_summary_length(self, summaries):
+    def compute_avg_summary_length(self, summaries: list[str]):
+        """
+        
+        """
         summary_lengths = []
         for summary in summaries:
             if self.is_valid_summary(summary):
@@ -35,6 +51,9 @@ class Metrics:
         return avg_summary_length
 
     def is_valid_summary(self, summary: str):
+        """
+
+        """
         if len(summary.split()) >= 5:
             return True
         else:
