72ae7412b42d29ca87274f665e322131fe63f4dc
ahmed <ahmed@vectara.com>
2025-05-27 11:51:23 -0700

    Metric setup

--- src/metrics/Metrics.py
+++ src/metrics/Metrics.py
@@ -2,3 +2,15 @@
 class Metrics:
     def __init__(self):
         pass
+
+    def hallucination_rate(self):
+        pass
+
+    def factual_consistancy_rate(self):
+        pass
+
+    def answer_rate(self):
+        pass
+
+    def avg_summary_length(self):
+        pass
