2a8b722013eedde9c3c384bc2bf837f07a022735
ahmed <ahmed@vectara.com>
2025-05-28 12:25:02 -0700

    Added Fanar

---
+++ src/LLMs/Fanar.py
@@ -0,0 +1,28 @@
+from src.LLMs.AbstractLLM import AbstractLLM
+from openai import OpenAI
+import os
+
+class Fanar(AbstractLLM):
+
+    def __init__(self, model_name, data_code=None):
+        super().__init__(model_name=model_name, company="Fanar")
+        self.api_key = os.getenv("FANAR_API_KEY")
+        self.client = OpenAI(
+            base_url="https://api.fanar.qa/v1",
+            api_key=self.api_key
+        )
+
+    def summarize(self, prepared_text: str) -> str:
+        chat_package = self.client.chat.completions.create(
+            model=self.model_name,
+            temperature=0,
+            messages=[{"role": "user", "content":prepared_text}]
+        )
+        summary = chat_package.choices[0].message.content
+        return summary
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
