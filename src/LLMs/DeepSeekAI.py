457ef4bc476df1e5889674c03b49973f6c0eb25f
ahmed <ahmed@vectara.com>
2025-05-29 12:45:23 -0700

    DeepSeek swapped to API call instead

--- src/LLMs/DeepSeekAI.py
+++ src/LLMs/DeepSeekAI.py
@@ -1,24 +1,23 @@
 from src.LLMs.AbstractLLM import AbstractLLM
-from transformers import pipeline
-import torch
-import gc
-import os
+from huggingface_hub import InferenceClient
+import re
 
 class DeepSeekAI(AbstractLLM):
 
     def __init__(self, model_name, data_code=None):
         super().__init__(model_name=model_name, company="deepseek-ai")
         self.full_model_name = f"{self.company}/{self.model_name}"
-        self.model = None
+        self.client = InferenceClient(model=self.full_model_name)
 
     def summarize(self, prepared_text: str) -> str:
-        summary = self.model(prepared_text, temperature=0)[0]['generated_text']
+        messages = [{"role": "user", "content":prepared_text}]
+        client_package = self.client.chat_completion(messages, temperature=0.0)
+        raw_summary = client_package.choices[0].message.content
+        summary = re.sub(r'<think>.*?</think>\s*', '', raw_summary, flags=re.DOTALL)
         return summary
 
     def setup(self):
-        self.model = pipeline("text-generation", model=self.full_model_name, device=0)
+        pass
 
     def teardown(self):
-        del self.model
-        gc.collect()
-        torch.cuda.empty_cache()
+        pass
