f1ac056bdf46c25ce2d4219a1b6bb7fcf64125eb
ahmed <ahmed@vectara.com>
2025-06-03 14:00:05 -0700

    Removing commented code

--- src/scripts/get_results.py
+++ src/scripts/get_results.py
@@ -35,24 +35,11 @@ def run(models: list[AbstractLLM]):
 
         logger.log(f"Generating results for {model_name}")
 
-        # summaries_json_file = f"summaries_{model_name}.json"
-        # summaries_json_path = os.path.join(model_out_dir, summaries_json_file)
         hhem_json_file = f"hhem_scores_{model_name}.json"
         hhem_json_path = os.path.join(model_out_dir, hhem_json_file)
 
-        # if json_exists(summaries_json_path) and json_exists(hhem_json_path):
         if json_exists(hhem_json_path):
             logger.log(f"Summary and HHEM JSON found for {model_name}")
-            # summaries_json = load_json(summaries_json_path)
-            # summaries_df = pd.DataFrame(summaries_json["summaries"])
-            # hhem_json = load_json(hhem_json_path)
-            # if len(hhem_df) != len(summaries_df):
-            #     logger.log("HHEM Summaries data length mismatch, skipping model")
-            #     continue
-            # hhem_summaries_df = pd.merge(
-            #     hhem_df, summaries_df,
-            #     on='article_id', how='inner'
-            # )
 
             results_json_file = f"results_{model_name}.json"
             results_json_path = os.path.join(model_out_dir, results_json_file)
