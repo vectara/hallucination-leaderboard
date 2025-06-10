48b40915a5662884caa40ec416246d39530223a8
ahmed <ahmed@vectara.com>
2025-06-10 10:34:47 -0700

    Analytics test setup

---
+++ src/tests/TestAnalytics.py
@@ -0,0 +1,24 @@
+from src.tests.AbstractTest import AbstractTest
+
+class TestAnalytics(AbstractTest):
+    def __init__(self):
+        pass
+
+    def run_tests(self):
+        pass
+
+    def test_hallucination_rate(self):
+        #TODO: Doc
+        pass
+
+    def test_answer_rate(self):
+        #TODO: Doc
+        pass
+
+    def test_avg_summary_length(self):
+        #TODO: Doc
+        pass
+
+    def test_is_valid_summary(self):
+        #TODO: Doc
+        pass
