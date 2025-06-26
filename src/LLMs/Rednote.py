902adcf94c04d47884f727d8e42bc75a64357244
ahmed <ahmed@vectara.com>
2025-06-26 11:53:22 -0700

    Rednote dots.llm1.inst added

---
+++ src/LLMs/Rednote.py
@@ -0,0 +1,85 @@
+from src.LLMs.AbstractLLM import AbstractLLM, EMPTY_SUMMARY
+import os
+import anthropic
+from src.LLMs.model_registry import register_model
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+
+COMPANY = "rednote"
+@register_model(COMPANY)
+class Rednote(AbstractLLM):
+    """
+    Class for models from rednote
+
+    Attributes:
+        client (Client): client associated with api calls with anthropic
+        model (str): rednote style model name
+    """
+
+    local_model_category = []
+
+    model_category1 = ["rednote-hilab/dots.llm1.inst"]
+
+    def __init__(
+            self,
+            model_name: str,
+            date_code: str,
+            temperature: float,
+            max_tokens: int,
+            thinking_tokens: int,
+            min_throttle_time: float
+    ):
+        super().__init__(
+            model_name,
+            date_code,
+            temperature,
+            max_tokens,
+            thinking_tokens,
+            min_throttle_time,
+            company=COMPANY
+        )
+        api_key = os.getenv("REDNOTE_API_KEY")
+        self.model = self.get_model_identifier(model_name, date_code)
+        if self.model_name not in self.local_model_category:
+            self.client = anthropic.Client(api_key=api_key)
+        else:
+            self.client = None
+
+    def summarize(self, prepared_text: str) -> str:
+        summary = EMPTY_SUMMARY
+        if self.model_name in self.model_category1 and self.client:
+            tokenizer = AutoTokenizer.from_pretrained(self.model)
+
+            model = AutoModelForCausalLM.from_pretrained(
+                self.model, device_map="auto", torch_dtype=torch.bfloat16
+            )
+
+            messages = [
+            {"role": "user", "content": prepared_text}
+            ]
+            input_tensor = tokenizer.apply_chat_template(
+                messages, add_generation_prompt=True, return_tensors="pt"
+            )
+            outputs = model.generate(
+                input_tensor.to(model.device), max_new_tokens=200
+            )
+
+            result = tokenizer.decode(
+                outputs[0][input_tensor.shape[1]:], skip_special_tokens=True
+            )
+            summary = result
+        else:
+            raise ValueError(f"Unsupported model: {self.model_name}")
+        return summary
+
+    def setup(self):
+        if self.model_name in self.local_model_category:
+            pass
+        else:
+            pass
+
+    def teardown(self):
+        if self.model_name in self.local_model_category:
+            pass
+        else:
+            pass
