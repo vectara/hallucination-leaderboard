e5efed79fd6a0501d66de71728d3d4fee0bba30c
ahmed <ahmed@vectara.com>
2025-07-10 11:42:54 -0700

    meta class added with no models

---
+++ src/LLMs/Meta.py
@@ -0,0 +1,74 @@
+import os
+from typing import Literal
+
+from . AbstractLLM import AbstractLLM
+from .. data_model import BasicLLMConfig, BasicSummary, BasicJudgment
+from .. data_model import ModelInstantiationError, SummaryError
+
+COMPANY = "meta"
+class MetaConfig(BasicLLMConfig):
+    """Extended config for Meta-specific properties"""
+    company: Literal["meta"] 
+    model_name: Literal["na"] # Only model names manually added to this list are supported.
+    date_code: str # do we need date code?
+    execution_mode: Literal["api"] = "api" # Is this company only API based?
+
+class MetaSummary(BasicSummary):
+    pass # Nothing additional to the BasicSummary class.
+
+class MetaLLM(AbstractLLM):
+    """
+    Class for models from Meta
+    """
+
+    # In which way to run the model via web api. Empty dict means not supported for web api execution. 
+    client_mode_group = {}
+
+    # In which way to run the model on local GPU. Empty dict means not supported for local GPU execution
+    local_mode_group = {}
+
+    def __init__(self, config: MetaConfig):
+        super().__init__(config)
+
+    def summarize(self, prepared_text: str) -> str:
+        summary = SummaryError.EMPTY_SUMMARY
+        if self.client:
+            match self.client_mode_group[self.model_name]:
+                case 1:
+                    summary = None
+        elif self.local_model: 
+            pass
+        else:
+            raise Exception(
+                ModelInstantiationError.MISSING_SETUP.format(
+                    class_name=self.__class__.__name__
+                )
+            )
+        return summary
+
+    def setup(self):
+        if self.execution_mode == "api":
+            if self.model_name in self.client_mode_group:
+                api_key = os.getenv(f"{COMPANY.upper()}_API_KEY")
+                assert api_key is not None, f"{COMPANY} API key not found in environment variable {COMPANY.upper()}_API_KEY"
+                self.client = None
+            else:
+                raise Exception(
+                    ModelInstantiationError.CANNOT_EXECUTE_IN_MODE.format(
+                        model_name=self.model_name,
+                        company=self.company,
+                        execution_mode=self.execution_mode
+                    )
+                )
+        elif self.execution_mode == "local":
+            pass
+
+    def teardown(self):
+        if self.client:
+            self.close_client()
+        elif self.local_model:
+            # self.default_local_model_teardown()
+            pass
+
+    def close_client(self):
+        pass
