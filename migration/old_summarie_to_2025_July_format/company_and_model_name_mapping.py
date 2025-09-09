49910686cf73a1f4f42b81e387335080e0d066fb
ahmed <ahmed@vectara.com>
2025-09-09 13:26:10 -0700

    Migration mapping in progress

--- migration/old_summarie_to_2025_July_format/company_and_model_name_mapping.py
+++ migration/old_summarie_to_2025_July_format/company_and_model_name_mapping.py
@@ -6,17 +6,69 @@
 # 2. For mistral models, this offical model listing has 
 
 # Only models that are mentioned below will be migrated. Others will not, mostly because of missing date code. They will be re-evaluated later.
+
+# Full List
+    # 01-ai - Correct
+        # Models Correct
+    # ai21labs - Correct
+        # Model names incorrect (Done)
+    # allenai - Correct
+        # Model names incorrect (DONE)
+    # amazon - Correct
+        # Model Names Incorrect (Done)
+    # anthropic - Correct
+        # Model Names incorrect (Done)
+    # apple - Correct
+        # Models Correct
+    # !!! cohere -> CohereLabs
+        # Model names incorrect (Done)
+    # databricks - Correct
+    # !!! deepseek -> deepseek-ai
+    # google - Correct
+    # ibm-granite - Correct
+    # intel - Correct
+    # internlm - Correct
+    # meta-llama - Correct
+    # microsoft - Correct
+    # mistralai - Correct
+    # openai - Correct
+    # qwen - Correct
+    # snowflake - Correct
+    # thudm - Correct
+    # tiiuae - Correct
+    # !!! xai -> xai-org
+
 name_mapping = {
     'xai' : 'xai-org',
+    'cohere': 'CohereLabs',
+    'deepseek': 'deepseek-ai',
 
-    'CohereForAI/c4ai-command-r-plus' : 'CohereLabs/c4ai-command-r-plus', # Open source on HF
-    'cohere/Cohere': 'CohereLabs/command', # commerical 
-    'cohere/command-r-plus-08-2024': 'CohereLabs/command-r-plus-08-2024',
-    'cohere/command-a-03-2025': 'CohereLabs/command-a-03-2025', # Open source on HF
+    'ai21labs/AI21-Jamba-1.5-Mini': 'ai21labs/AI21-Jamba-Mini-1.5',
+    'ai21labs/jamba-1.6-large': 'ai21labs/AI21-Jamba-Large-1.6',
+    'ai21labs/jamba-1.6-mini': 'ai21labs/AI21-Jamba-Mini-1.6',
+
+    'allenai/olmo-2-0325-32b-instruct': 'allenai/OLMo-2-0325-32B-Instruct',
+
+    'amazon/nova-lite-v1': 'amazon/nova-lite-v1:0',
+    'amazon/nova-micro-v1': 'amazon/nova-micro-v1:0',
+    'amazon/nova-pro-v1': 'amazon/nova-pro-v1:0',
+    'amazon/Titan-Express': 'amazon/titan-text-express-v1', # MAYBE: Ambiguous can skip
+
+    'anthropic/Claude-2': 'anthropic/claude-2.0',
+    'anthropic/claude-3-5-sonnet': 'anthropic/claude-3-5-sonnet-20241022',
+    'anthropic/claude-3-7-sonnet-latest': 'ambiguous',
+    'anthropic/claude-3-7-sonnet-latest-think': 'ambiguous',
+    'anthropic/Claude-3-opus': 'anthropic/claude-3-opus-20240229',
+    'anthropic/Claude-3-sonnet': 'anthropic/claude-3-sonnet-20240229',
+
     'cohere/c4ai-aya-expanse-8b': 'CohereLabs/aya-expanse-8b', # Open source on HF
     'cohere/c4ai-aya-expanse-32b': 'CohereLabs/aya-expanse-32b', # Open source on HF
+    'CohereForAI/c4ai-command-r-plus' : 'CohereLabs/c4ai-command-r-plus', # Open source on HF
+    'cohere/Cohere': 'CohereLabs/command', # commerical
     'cohere/Cohere-Chat': 'CohereLabs/command-chat', # the same Cohere Command model but using the /chat endpoint
-    'cohere/command-r-08-2024': 'CohereLabs/command-r-08-2024', # Open source on HF
+    'cohere/command-a': 'CohereLabs/command-a-03-2025', # Open source on HF
+    'cohere/command-r': 'CohereLabs/command-r-08-2024', # Open source on HF
+    'cohere/command-r-plus': 'CohereLabs/command-r-plus-08-2024',
 
     'mistralai/mistral-small-3.1-24b-instruct': 'mistralai/Mistral-Small-3.1-24b-instruct-2503',  # I cannot find the model mistral-small-3.1-24b-instruct in google. the closest I can find is mistral-small-3.1-24b-instruct-2503. 
     'mistralai/Mixtral-8x22B-Instruct-v0.1': 'mistralai/Mixtral-8x22B-Instruct-v0.1',
