57fc653fc403ac3c0a542e36f42a5bc52d56e3f0
Forrest Bao <forrest.bao@gmail.com>
2025-09-09 00:17:12 +0000

    use name_mapping to indicate what LLMs to migrate

--- migration/old_summarie_to_2025_July_format/company_and_model_name_mapping.py
+++ migration/old_summarie_to_2025_July_format/company_and_model_name_mapping.py
@@ -5,8 +5,8 @@
 # 1. For cohere/Cohere and cohere/Cohere-Chat, see old README https://github.com/vectara/hallucination-leaderboard/tree/hhem-1.0-final?tab=readme-ov-file#cohere-models
 # 2. For mistral models, this offical model listing has 
 
-
-mapping = {
+# Only models that are mentioned below will be migrated. Others will not, mostly because of missing date code. They will be re-evaluated later.
+name_mapping = {
     'xai' : 'xai-org',
 
     'CohereForAI/c4ai-command-r-plus' : 'CohereLabs/c4ai-command-r-plus', # Open source on HF
@@ -19,17 +19,17 @@ mapping = {
     'cohere/command-r-08-2024': 'CohereLabs/command-r-08-2024', # Open source on HF
 
     'mistralai/mistral-small-3.1-24b-instruct': 'mistralai/Mistral-Small-3.1-24b-instruct-2503',  # I cannot find the model mistral-small-3.1-24b-instruct in google. the closest I can find is mistral-small-3.1-24b-instruct-2503. 
-    # 'mistralai/Mixtral-8x22B-Instruct-v0.1', # On HF, no need to rename
+    'mistralai/Mixtral-8x22B-Instruct-v0.1': 'mistralai/Mixtral-8x22B-Instruct-v0.1',
     # 'mistralai/mistral-small-latest', # I cannot determine the version or date code. Mistral-small has v.2, v.3.x.
-    # 'mistralai/Mistral-Nemo-Instruct-2407', # On HF, no need to rename
+    'mistralai/Mistral-Nemo-Instruct-2407': 'mistralai/Mistral-Nemo-Instruct-2407',
     'mistralai/ministral-8b-latest': 'mistralai/Ministral-8B-Instruct-2410',  # there seems to be only one ministral-8b. So i assume it is this one
-    # 'mistralai/Mixtral-8x7B-Instruct-v0.1', # On HF, no need to rename
+    'mistralai/Mixtral-8x7B-Instruct-v0.1': 'mistralai/Mixtral-8x7B-Instruct-v0.1',
     # 'mistralai/mistral-large-latest', # I cannot determine the version or date code. But 
     # 'mistralai/Mistral-Large2', # I cannot determine the version or date code.
-    # 'mistralai/Mistral-Small-24B-Instruct-2501', # On HF, no need to rename
+    'mistralai/Mistral-Small-24B-Instruct-2501': 'mistralai/Mistral-Small-24B-Instruct-2501',
     # 'mistralai/ministral-3b-latest', # I cannot find this model. There is only ministral-3b-instruct. 
-    # 'mistralai/Mistral-7B-Instruct-v0.3', # On HF, no need to rename
-    'mistralai/pixtral-large-latest': 'mistralai/Pixtral-Large-Instruct-2411' # there is only one version pixal-large so far, which is 2411
+    'mistralai/Mistral-7B-Instruct-v0.3': 'mistralai/Mistral-7B-Instruct-v0.3',
+    'mistralai/pixtral-large-latest': 'mistralai/Pixtral-Large-Instruct-2411', # there is only one version pixal-large so far, which is 2411
 
 
     'gemini-2.0-flash-exp' : 'google/gemini-2.0-flash-exp',
