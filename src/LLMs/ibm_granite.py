a023b78a870db592e8eb344f1097e996ea6175b6
ahmed <ahmed@vectara.com>
2025-10-03 16:09:48 -0700

    still working on making micro work

--- src/LLMs/ibm_granite.py
+++ src/LLMs/ibm_granite.py
@@ -131,7 +131,6 @@ class IBMGraniteLLM(AbstractLLM):
                     output = self.local_model.generate(
                         **input_tokens, 
                         max_new_tokens=self.max_tokens,
-                        temperature=self.temperature
                     )
                     output = tokenizer.batch_decode(output)
                     summary = output
