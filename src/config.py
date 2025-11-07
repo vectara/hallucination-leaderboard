4aad3a9156b86344b5a701e08baf1aff89a49575
ahmed <ahmed@vectara.com>
2025-11-06 17:06:03 -0800

    config update

--- src/config.py
+++ src/config.py
@@ -556,6 +556,26 @@ Here is the passage:
         #   }
         # ),
 
+        # CPU 5
+        # Mockingbird
+
+        # Below are verifiction runs
+        # CPU 6
+        # AnthropicConfig(**{"company": "anthropic", "model_name": "claude-sonnet-4-5", "date_code": "20250929", "temperature": 0.0}),
+
+        # CPU 7
+        # OpenAIConfig(**{"company": "openai", "model_name": "gpt-4.1", "date_code": "2025-04-14", "temperature": 0.0}),
+
+        # CPU 8
+        # DeepSeekAIConfig(**{"company": "deepseek-ai", "model_name": "DeepSeek-V3.2-Exp", "temperature": 0.0, "min_throttle_time": 4.0}),
+
+        # CPU 9
+        # GoogleConfig(**{"company": "google", "model_name": "gemini-2.5-flash-lite", "date_code": "", "temperature": 0.0, "thinking_budget": 0}),
+
+        # CPU 10
+        # XAIConfig(**{"model_name": "grok-4-fast-non-reasoning", "temperature": 0.0, "min_throttle_time": 2.0}),
+
+
         # # Completed Models
         # XAIConfig(**{"model_name": "grok-4-fast-reasoning", "temperature": 0.0, "min_throttle_time": 2.0}),
 
