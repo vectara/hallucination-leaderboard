2f41006a84a3bd5debf84179ea62710c621c6c3f
ahmed <ahmed@vectara.com>
2025-12-09 14:23:43 -0800

    update example company

--- src/LLMs/__example_company.py
+++ src/LLMs/__example_company.py
@@ -7,20 +7,19 @@ from .. data_model import ModelInstantiationError, SummaryError
 
 # TODO: IMPORTS IF NEEDED
 
-COMPANY = "COMPANY_NAME" # TODO: Provice EXACT company name
-class COMPANY_NAMEConfig(BasicLLMConfig): # TODO: Edit name
-    """Extended config for COMPANY_NAME-specific properties""" # TODO: Update comment
+COMPANY = "COMPANY_NAME" # TODO: Using name from huggingface
+class COMPANY_NAMEConfig(BasicLLMConfig): # TODO: Update object name
     company: Literal["COMPANY_NAME"] = "COMPANY_NAME" # TODO: Same name as COMPANY
     model_name: Literal[
         "MODEL_NAME", # TODO: Add models
-    ] # Only model names manually added to this list are supported.
+    ]
     date_code: str = ""
-    execution_mode: Literal["api", "cpu", "gpu"] = "api" # GPU refers to a local model
+    execution_mode: Literal["api", "cpu", "gpu"] = "api"
     endpoint: Literal["chat", "response"] = "chat"
     # TODO: Add new attributes if needed for further customization
 
 class COMPANY_NAMESummary(BasicSummary): # TODO: Update object name
-    endpoint: Literal["chat", "response"] | None = None # No default. Needs to be set from from LLM config.
+    endpoint: Literal["chat", "response"] | None = None
 
     class Config:
         extra = "ignore"
@@ -30,32 +29,38 @@ class COMPANY_NAMELLM(AbstractLLM): # TODO: Update object name
     Class for models from COMPANY_NAME
     """
 
-    # In which way to run the model via web api. Empty dict means not supported for web api execution. 
+    # TODO: Add API models here to specify what logic path to run that model from
     client_mode_group = {
-        "MODEL_NAME": { # TODO: Add API models here to specify what logic path to run that model from
+        "MODEL_NAME": { 
             "chat": 1
         }
     }
 
-    # In which way to run the model on local GPU. Empty dict means not supported for local GPU execution
-    local_mode_group = {} #TODO: Add local models here and specify what logic path to run that model
+    # TODO: Add local models here and specify what logic path to run that model
+    local_mode_group = {
+        "MODEL_NAME": {
+            "chat": 1
+        }
+    } 
 
-    def __init__(self, config: COMPANY_NAMEConfig): # TODO: Update config
+    def __init__(self, config: COMPANY_NAMEConfig): # TODO: Update config name
         super().__init__(config)
         self.endpoint = config.endpoint
         self.execution_mode = config.execution_mode
         self.full_config = config
 
     def summarize(self, prepared_text: str) -> str:
-        # Use self.model_fullname when referring to the model
         summary = SummaryError.EMPTY_SUMMARY
         if self.client:
             match self.client_mode_group[self.model_name][self.endpoint]:
-                #TODO Define how the case 1 model will run
+                # TODO Define how the case 1 model will run
                 case 1:
-                    summary = None 
+                    pass
         elif self.local_model: 
-            pass
+            match self.local_mode_group[self.model_name][self.endpoint]:
+                # TODO Define how the case 1 model will run
+                case 1:
+                    pass
         else:
             raise Exception(
                 ModelInstantiationError.MISSING_SETUP.format(
@@ -72,7 +77,8 @@ class COMPANY_NAMELLM(AbstractLLM): # TODO: Update object name
                     f"{COMPANY} API key not found in environment variable "
                     f"{COMPANY.upper()}_API_KEY"
                 )
-                self.client = None # TODO: Assign client if using client based models
+                # TODO: Assign client if using client based models
+                self.client = None 
             else:
                 raise Exception(
                     ModelInstantiationError.CANNOT_EXECUTE_IN_MODE.format(
@@ -82,15 +88,22 @@ class COMPANY_NAMELLM(AbstractLLM): # TODO: Update object name
                     )
                 )
         elif self.execution_mode == "local":
-            # TODO: Assign a local model if using a local model
-            self.local_model = None
-            pass
+            if self.model_name in self.local_mode_group:
+                # TODO: Assign a local model if using a local model
+                self.local_model = None
+            else:
+                raise Exception(
+                    ModelInstantiationError.CANNOT_EXECUTE_IN_MODE.format(
+                        model_name=self.model_name,
+                        company=self.company,
+                        execution_mode=self.execution_mode
+                    )
+                )
 
     def teardown(self):
         if self.client:
-            self.close_client()
+            pass
         elif self.local_model:
-            # self.default_local_model_teardown()
             pass
 
     def close_client(self):
