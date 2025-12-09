9157e51198cd889236b0c3383d17273699665c8f
ahmed <ahmed@vectara.com>
2025-12-09 14:26:46 -0800

    databricks cleanup

--- src/LLMs/databricks.py
+++ src/LLMs/databricks.py
@@ -5,19 +5,18 @@ from . AbstractLLM import AbstractLLM
 from .. data_model import BasicLLMConfig, BasicSummary, BasicJudgment
 from .. data_model import ModelInstantiationError, SummaryError
 
-COMPANY = "databricks" #Official company name on huggingface
+COMPANY = "databricks"
 class DatabricksConfig(BasicLLMConfig):
-    """Extended config for databricks-specific properties"""
     company: Literal["databricks"] = "databricks"
     model_name: Literal[
         "dbrx-instruct",
-    ] # Only model names manually added to this list are supported.
+    ]
     date_code: str = ""
     execution_mode: Literal["api", "cpu", "gpu"] = "api"
     endpoint: Literal["chat", "response"] = "chat"
 
 class DatabricksSummary(BasicSummary):
-    endpoint: Literal["chat", "response"] | None = None # No default. Needs to be set from from LLM config.
+    endpoint: Literal["chat", "response"] | None = None
 
     class Config:
         extra = "ignore"
@@ -27,14 +26,12 @@ class DatabricksLLM(AbstractLLM):
     Class for models from databricks
     """
 
-    # In which way to run the model via web api. Empty dict means not supported for web api execution. 
     client_mode_group = {
         "dbrx-instruct": {
             "chat": 1
         }
     }
 
-    # In which way to run the model on local GPU. Empty dict means not supported for local GPU execution
     local_mode_group = {}
 
     def __init__(self, config: DatabricksConfig):
@@ -44,7 +41,6 @@ class DatabricksLLM(AbstractLLM):
         self.full_config = config
 
     def summarize(self, prepared_text: str) -> str:
-        # Use self.model_fullname when referring to the model
         summary = SummaryError.EMPTY_SUMMARY
         if self.client:
             match self.client_mode_group[self.model_name][self.endpoint]:
@@ -82,9 +78,8 @@ class DatabricksLLM(AbstractLLM):
 
     def teardown(self):
         if self.client:
-            self.close_client()
+            pass
         elif self.local_model:
-            # self.default_local_model_teardown()
             pass
 
     def close_client(self):
